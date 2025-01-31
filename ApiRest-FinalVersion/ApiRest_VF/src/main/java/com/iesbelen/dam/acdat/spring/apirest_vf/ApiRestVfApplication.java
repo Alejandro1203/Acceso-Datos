9ad26923ff288815a7ed24a9fc226600e3b866a4
Alejandro1203 <alefg2003@gmail.com>
2025-01-31 12:03:21 +0100

    ApiRest_VF Creacion

---
+++ ApiRest-FinalVersion/ApiRest_VF/src/main/java/com/iesbelen/dam/acdat/spring/apirest_vf/ApiRestVfApplication.java
@@ -0,0 +1,13 @@
+package com.iesbelen.dam.acdat.spring.apirest_vf;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestVfApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiRestVfApplication.class, args);
+    }
+
+}
---
+++ ApiRest-FinalVersion/ApiRest_VF/src/test/java/com/iesbelen/dam/acdat/spring/apirest_vf/ApiRestVfApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iesbelen.dam.acdat.spring.apirest_vf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiRestVfApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
